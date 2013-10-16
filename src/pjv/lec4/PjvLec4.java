508df144cfe05a68ab338e47c4b78852cfaa598a
Lubomir Cuhel <cuhellub@fit.cvut.cz>
2013-10-16 15:22:50 +0200

    student List and Set test, method equals

--- src/pjv/lec4/PjvLec4.java
+++ src/pjv/lec4/PjvLec4.java
@@ -4,6 +4,11 @@
  */
 package pjv.lec4;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  *
  * @author cuhellub
@@ -17,5 +22,21 @@ public class PjvLec4 {
         Student student1 = new Student("Tomas", "Marny", 1234, 2.1);
         Student student2 = new Student("Tomas", "Fuk", 45678, 1.3);
         Student student3 = new Student("Honza", "Novak", 1234, 4.2);
+        
+        List<Student> list = new ArrayList<Student>();
+        Set<Student> set = new HashSet<Student>();
+        
+        list.add(student1);
+        list.add(student2);
+        list.add(student3);
+        list.add(student1);
+        
+        set.add(student1);
+        set.add(student2);
+        set.add(student3);
+        set.add(student1);
+        
+        System.out.println(list);
+        System.out.println(set);
     }
 }
--- src/pjv/lec4/Student.java
+++ src/pjv/lec4/Student.java
@@ -35,6 +35,26 @@ public class Student {
     public double getMarksAvg() {
         return marksAvg;
     }
+
+    @Override
+    public String toString() {
+        return "Student{" + "name=" + name + ", surname=" + surname + ", ID=" + ID + ", marksAvg=" + marksAvg + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Student other = (Student) obj;
+        if (this.ID != other.ID) {
+            return false;
+        }
+        return true;
+    }
     
     
 }
