6bb63b58af798536948392429f445dcf89e38b81
Lubomir Cuhel <cuhellub@fit.cvut.cz>
2013-10-16 15:55:27 +0200

    School class implemented

--- src/pjv/lec4/School.java
+++ src/pjv/lec4/School.java
@@ -4,10 +4,18 @@
  */
 package pjv.lec4;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  *
  * @author cuhellub
  */
 public class School {
+    private Map<Integer, List<Student>> grades;
+
+    public School(Map<Integer, List<Student>> grades) {
+        this.grades = grades;
+    }    
     
 }
